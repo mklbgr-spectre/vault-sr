539a27156f8c7901bf91d1ebed593a13ea020e96
mklbgr-spectre <mklbgr@gmail.com>
2026-03-21 06:05:05 +0100

    fix base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "mklbgr-spectre.github.io/vault-sr.",
+    baseUrl: "mklbgr-spectre.github.io/vault-sr",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
